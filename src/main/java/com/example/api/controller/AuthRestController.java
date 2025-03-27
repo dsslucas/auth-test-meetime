557f7447a540c39c2f61774852632d05c24b90dd
dsslucas <lucasdasilvasou@gmail.com>
2025-03-27 20:44:29 -0300

    feat: call to Hubspot and receiving code

--- src/main/java/com/example/api/controller/AuthRestController.java
+++ src/main/java/com/example/api/controller/AuthRestController.java
@@ -1,6 +1,10 @@
 package com.example.api.controller;
 
 import com.example.api.model.dto.AuthRequestDto;
+import com.example.api.service.AuthService;
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +12,16 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 public class AuthRestController {
+    @Value("${app.api.client_id}")
+    private String clientId;
+
+    @Value("${app.api.redirect_uri}")
+    private String redirectUri;
+
+    @Value("${app.api.scope}")
+    private String scope;
+
+    private final AuthService authService = new AuthService();
 
     @GetMapping("/test")
     public ResponseEntity<String> test() {
@@ -15,8 +29,17 @@ public class AuthRestController {
     }
 
     @PostMapping("/")
+    public ResponseEntity<Void> authUser() {
+        String url = authService.authUser(clientId, scope, redirectUri);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", url);
+
+        return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+    }
+
+    @GetMapping("/getCode")
     @ResponseStatus(HttpStatus.OK)
-    public String authUser(@RequestBody AuthRequestDto body){
-        return "";
+    public String getToken(@RequestParam("code") String code){
+        return authService.getToken(code);
     }
 }
--- src/main/java/com/example/api/service/AuthService.java
+++ src/main/java/com/example/api/service/AuthService.java
@@ -1,11 +1,30 @@
 package com.example.api.service;
 
-import com.example.api.model.dto.AuthRequestDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class AuthService {
-    public String authUser(AuthRequestDto data){
-        return "";
+    public String authUser(String clientId, String scope, String redirectUri){
+        String AUTH_URL = "https://app.hubspot.com/oauth/authorize";
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        String url = AUTH_URL + "?client_id=" + clientId + "&scope=" + scope + "&redirect_uri=" + redirectUri;
+
+        System.out.println(url);
+        return url;
+    }
+
+    public String getToken(String code){
+        System.out.println("token obtido: " + code);
+        return code;
     }
 }
