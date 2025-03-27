30edd965389c51ead6c049709b9a80377b18c994
dsslucas <lucasdasilvasou@gmail.com>
2025-03-27 16:10:48 -0300

    feat: first commit

---
+++ src/main/java/com/example/api/AuthTestMeetimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthTestMeetimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthTestMeetimeApplication.class, args);
+	}
+
+}
