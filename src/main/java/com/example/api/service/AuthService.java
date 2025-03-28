fc31815d1496ad5cd8f234414081d5e62b177b42
dsslucas <lucasdasilvasou@gmail.com>
2025-03-28 15:46:00 -0300

    refactor: reposicioning token set

--- src/main/java/com/example/api/service/AuthService.java
+++ src/main/java/com/example/api/service/AuthService.java
@@ -1,11 +1,17 @@
 package com.example.api.service;
 
 import com.example.api.model.dto.auth.AuthResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.*;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class AuthService {
@@ -16,7 +22,7 @@ public class AuthService {
         return url;
     }
 
-    public AuthResponseDto getToken(String clientId, String clientSecret, String code){
+    public AuthResponseDto getToken(String clientId, String clientSecret, String code, HttpServletRequest request){
         String url = "https://api.hubapi.com/oauth/v1/token";
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
@@ -30,6 +36,17 @@ public class AuthService {
 
         HttpEntity<String> requestEntity = new HttpEntity<>(body, headers);
         ResponseEntity<AuthResponseDto> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, AuthResponseDto.class);
+
+        if(response.getBody() != null && response.getBody().access_token() != null){
+            // Save manually the token of session
+            request.getSession().setAttribute("ACCESS_TOKEN", response.getBody().access_token());
+
+            // Define the authenticated user on Spring Security scope
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(response.getBody().access_token(), null, List.of(new SimpleGrantedAuthority("ROLE_USER")));
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
 
         return response.getBody();
     }
