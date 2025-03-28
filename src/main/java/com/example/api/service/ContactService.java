1bd48ff393e047c409d5ae7cd1f59826a9790ad3
dsslucas <lucasdasilvasou@gmail.com>
2025-03-28 16:09:41 -0300

    feat: save new contact

--- src/main/java/com/example/api/service/ContactService.java
+++ src/main/java/com/example/api/service/ContactService.java
@@ -2,7 +2,9 @@ package com.example.api.service;
 
 import com.example.api.model.dto.contact.CreateContactRequestDto;
 import com.example.api.model.dto.contact.PropertyContactDto;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ContactService {
@@ -10,10 +12,17 @@ public class ContactService {
     private final AuthService authService = new AuthService();
 
     public String createContact(PropertyContactDto data){
-        CreateContactRequestDto requestBody = new CreateContactRequestDto(data);
-
+        CreateContactRequestDto body = new CreateContactRequestDto(data);
+        String url = "https://api.hubapi.com/crm/v3/objects/contacts";
         String token = authService.getToken();
 
-        return "User created";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(token);
+
+        HttpEntity<CreateContactRequestDto> requestEntity = new HttpEntity<>(body, headers);
+        ResponseEntity<Object> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, Object.class);
+        return "User created!";
     }
 }
