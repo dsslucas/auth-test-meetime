36d186b71fde886e44d5672df72bbb80c365753e
dsslucas <lucasdasilvasou@gmail.com>
2025-03-29 11:38:08 -0300

    feat: validation on create contact

--- src/main/java/com/example/api/model/dto/contact/PropertyContactDto.java
+++ src/main/java/com/example/api/model/dto/contact/PropertyContactDto.java
@@ -1,8 +1,17 @@
 package com.example.api.model.dto.contact;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record PropertyContactDto(
+        @Email(message = "Invalid e-mail.")
+        @NotBlank(message = "Email not informed.")
         String email,
+
+        @NotBlank(message = "Firstname not informed.")
         String firstname,
+
+        @NotBlank(message = "Lastname not informed.")
         String lastname
 ) {
 }
