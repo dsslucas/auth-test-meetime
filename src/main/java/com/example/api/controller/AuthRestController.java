2b79aaad3dc6b1fa688ca6752e006bb99cefc6b6
dsslucas <lucasdasilvasou@gmail.com>
2025-03-28 21:14:04 -0300

    refactor: returning HTML for authenticate

--- src/main/java/com/example/api/controller/AuthRestController.java
+++ src/main/java/com/example/api/controller/AuthRestController.java
@@ -3,17 +3,10 @@ package com.example.api.controller;
 import com.example.api.model.dto.auth.AuthResponseDto;
 import com.example.api.service.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/auth")
@@ -38,18 +31,14 @@ public class AuthRestController {
     }
 
     @PostMapping("/")
-    public ResponseEntity<Void> authUser() {
-        String url = authService.authUser(clientId, scope, redirectUri);
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Location", url);
-
-        return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+    @ResponseStatus(HttpStatus.OK)
+    public String authUser() {
+        return authService.authUser(clientId, scope, redirectUri);
     }
 
     @GetMapping("/getCode")
     @ResponseStatus(HttpStatus.OK)
     public AuthResponseDto getToken(@RequestParam("code") String code, HttpServletRequest request) {
-        AuthResponseDto authResponse = authService.getToken(clientId, clientSecret, code, request);
-        return authResponse;
+        return authService.getToken(clientId, clientSecret, code, request);
     }
 }
