4088c4344812030c580053e62bdc891aa3ce132a
dsslucas <lucasdasilvasou@gmail.com>
2025-03-31 13:19:14 -0300

    feat: enable webhook

--- src/main/java/com/example/api/controller/ContactRestController.java
+++ src/main/java/com/example/api/controller/ContactRestController.java
@@ -3,7 +3,7 @@ package com.example.api.controller;
 import com.example.api.model.dto.contact.PropertyContactDto;
 import com.example.api.service.ContactService;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,5 +16,11 @@ public class ContactRestController {
     @ResponseStatus(HttpStatus.OK)
     public String createContact(@RequestBody @Validated PropertyContactDto data){
         return contactService.createContact(data);
+    }
+
+    @PostMapping(value = "/webhook", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public void receiveWebhook(@RequestBody Object webhookNotificationPayload) {
+        System.out.println("Webhook Notification Received: {}" + webhookNotificationPayload);
     }
 }
